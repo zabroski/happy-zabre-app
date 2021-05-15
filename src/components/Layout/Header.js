5eae2163e31a00b842924016ad9e1742365e0e4a
zabroski <35234595+zabroski@users.noreply.github.com>
2021-05-14 20:28:26 -0400

    added modal with cart components app lokking good. nice work

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,21 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import HeaderCartButton from "./HeaderCartButton"
-import mealsImage from "../../assets/meals.jpg"
-import classes from "./Header.module.css"
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = props => 
-{
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>HappyMeals</h1>
-            <HeaderCartButton />    
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="delicious food" />
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
     </Fragment>
-}
+  );
+};
 
 export default Header;
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,23 +1,35 @@
-// import MealItemForm from "./MealItemForm";
-// import MealItemForm from "./ MealItemForm ";
-import classes from "./MealItem.module.css";
+import { useContext } from 'react';
+
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
-    const price = `${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
 
-    return ( 
-        <li className={classes.meal}>
-            <div>
-                <h3>{props.name}</h3>
-                <div className={classes.description}>{props.description}</div>
-                <div className={classes.price}>{price}</div>
-            </div>
-            <div>
-                {/* <MealItemForm /> */}
-                Hererere
-            </div>
-        </li>
-    );
-}
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
 
 export default MealItem;
